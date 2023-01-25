ca29e2e706a94af32b32ae3cc3321d7c61278f0e
Tyluur <itstyluur@icloud.com>
2023-01-24 23:47:33 -0500

    fix: translate all local dependencies to maven central (+ move packaging to match expected file structure)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,62 +6,80 @@ plugins {
 val junitVersion = "5.6.2"
 val koinVersion = "2.1.5"
 
-allprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "idea")
-    apply(plugin = "org.jetbrains.kotlin.jvm")
+apply(plugin = "kotlin")
+apply(plugin = "idea")
+apply(plugin = "org.jetbrains.kotlin.jvm")
 
-    group = "dusk-rs"
-    version = "0.0.1"
+group = "RedRune"
+version = "0.0.1"
 
-    java.sourceCompatibility = JavaVersion.VERSION_15
+java.sourceCompatibility = JavaVersion.toVersion('8')
+java.targetCompatibility = JavaVersion.toVersion('8')
 
-    repositories {
-        mavenCentral()
-        mavenLocal()
-        jcenter()
-        maven(url = "https://repo.maven.apache.org/maven2")
-        maven(url = "https://jitpack.io")
-    }
+repositories {
+    mavenCentral()
+    mavenLocal()
+    jcenter()
+    maven(url = "https://repo.maven.apache.org/maven2")
+    maven(url = "https://jitpack.io")
 }
 
-subprojects {
-    dependencies {
-        // Jvm
-        implementation(kotlin("stdlib"))
+dependencies {
+    // Jvm
+    implementation(kotlin("stdlib"))
 
-        // Kotlin
-        implementation(kotlin("reflect"))
-        implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.2")
-        implementation("org.koin", "koin-logger-slf4j", koinVersion)
+    // Kotlin
+    implementation(kotlin("reflect"))
+    // Java
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation(kotlin("gradle-plugin", version = "1.5.0"))
 
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.4.2")
 
-        // Network
-        implementation("io.netty:netty-all:4.1.44.Final")
+    // Dependency Injection
+    implementation("org.koin", "koin-core", koinVersion)
+    implementation("org.koin", "koin-logger-slf4j", koinVersion)
 
-        // Logging
-        implementation("org.slf4j:slf4j-api:1.7.30")
-        implementation("ch.qos.logback:logback-classic:1.2.3")
+    // Network
+    implementation("io.netty:netty-all:4.1.86.Final")
 
-        // https://mvnrepository.com/artifact/org.cryptonode.jncryptor/jncryptor
-        implementation("org.cryptonode.jncryptor:jncryptor:1.2.0")
-
-        // https://mvnrepository.com/artifact/com.zaxxer/HikariCP
-        implementation("com.zaxxer:HikariCP:2.3.2")
-
-        compileOnly("org.projectlombok:lombok:1.16.10")
-
-        // Testing
-        testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    // Logging
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.2.9") {
+        exclude("org.slf4j", "slf4j-jdk14")
     }
+    implementation("com.michael-bull.kotlin-inline-logger", "kotlin-inline-logger-jvm", "1.0.2")
 
-    tasks {
-        compileKotlin {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        compileTestKotlin {
-            kotlinOptions.jvmTarget = "1.8"
-        }
+    // https://mvnrepository.com/artifact/org.cryptonode.jncryptor/jncryptor
+    implementation("org.cryptonode.jncryptor:jncryptor:1.2.0")
+
+    // https://mvnrepository.com/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:2.3.2")
+
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-lang3
+    implementation("org.apache.commons:commons-lang3:3.4")
+
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation("com.google.guava:guava:30.0-android")
+
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+    // Testing
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
     }
-
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
