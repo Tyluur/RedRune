6913f561f2c47bdc941c2f417013b6b5fc6db957
Tyluur <itstyluur@icloud.com>
2023-02-04 21:24:59 -0500

    chore: correct author tag in GameFlags.kt

--- src/main/java/org/redrune/game/GameFlags.kt
+++ src/main/java/org/redrune/game/GameFlags.kt
@@ -3,7 +3,7 @@ package org.redrune.game
 /**
  * The flags that can be altered for the game are stored here.
  *
- * @author Tyluur <itstyluur></itstyluur>@gmail.com>
+ * @author Tyluur <itstyluur@gmail.com>
  * @since 5/18/2017
  */
 object GameFlags {
